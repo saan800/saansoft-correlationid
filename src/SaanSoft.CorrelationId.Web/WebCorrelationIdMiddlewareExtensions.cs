59e933bc65962605992d7dff00012da07a7d0508
Sandra Greenhalgh <sandragreenhalgh@gmail.com>
2025-08-14 07:52:29 +0800

    Update WebCorrelationIdMiddlewareExtensions.cs

--- src/SaanSoft.CorrelationId.Web/WebCorrelationIdMiddlewareExtensions.cs
+++ src/SaanSoft.CorrelationId.Web/WebCorrelationIdMiddlewareExtensions.cs
@@ -5,14 +5,16 @@ namespace SaanSoft.CorrelationId.Web;
 public static class WebCorrelationIdMiddlewareExtensions
 {
     /// <summary>
-    /// Use the WebCorrelationIdMiddleware to configure the value supplied by ICorrelationIdProvider from web Http context and/por headers
+    /// Use the WebCorrelationIdMiddleware to configure the value supplied by ICorrelationIdProvider from HttpCntext. 
     /// It will use the default WebCorrelationIdOptions values.
     /// </summary>
+// TODO: cref for ^^
+// TODO: make options required, and this function ne up default 
     public static IApplicationBuilder UseWebCorrelationIdMiddleware(this IApplicationBuilder builder)
         => builder.UseMiddleware<WebCorrelationIdMiddleware>();
 
     /// <summary>
-    /// Use the WebCorrelationIdMiddleware to configure the value supplied by ICorrelationIdProvider from web Http context and/or headers
+    /// Use the WebCorrelationIdMiddleware to configure the value supplied by ICorrelationIdProvider from HttpContext.
     /// </summary>
     public static IApplicationBuilder UseWebCorrelationIdMiddleware(this IApplicationBuilder builder, WebCorrelationIdOptions options)
         => builder.UseMiddleware<WebCorrelationIdMiddleware>(options);
